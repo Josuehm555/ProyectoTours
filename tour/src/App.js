e57ff2cf4bc61ab90f72fd4156ddf38da2507f9a
Jeremmy Soto <jeremmysq17@gmail.com>
2022-08-26 19:33:32 -0600

    routes

--- tour/src/App.js
+++ tour/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Route from './route/route'
+import Menu from "./components/menu/menu";
+import MenuItems from './api/menu-items.json'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu  Items={MenuItems}/>
+      <Route/>
     </div>
   );
 }
-
-export default App;
