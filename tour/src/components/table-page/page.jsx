32e2be4687e75f239a4812cf57fbf3f748c11897
Jeremmy Soto <jeremmysq17@gmail.com>
2022-08-28 20:53:39 +0000

    Page Margins

--- tour/src/components/table-page/page.jsx
+++ tour/src/components/table-page/page.jsx
@@ -4,7 +4,7 @@ import Form from "../search/form";
 import Table from "../admin-table-3-columns/table";
 import './styles.css'
 
-export default function TablePage({Columns, Rows, Title}) {
+export default function TablePage({ Columns, Rows, Title }) {
     const originalList = Rows.data
     const [list, setList] = useState(originalList);
     const [length, setLength] = useState(originalList.length)
@@ -12,12 +12,14 @@ export default function TablePage({Columns, Rows, Title}) {
     return (
         <>
             <h1>{Title}</h1>
-            <div className="formContainer">
-                <Button titulo={Title}/>
-                <Form original={originalList} setList={setList} setLength={setLength} title={Title}/>
+            <div className="tableContainer">
+                <div className="formContainer">
+                    <Button titulo={Title}/>
+                    <Form original={originalList} setList={setList} setLength={setLength} title={Title} />
+                </div>
+                <Table columns={Columns} rows={list} length={length} />
+                {/* aqui va la paginacion */}
             </div>
-            <Table columns={Columns} rows={list} length={length} />
-            {/* aqui va la paginacion */}
         </>
     );
 }
