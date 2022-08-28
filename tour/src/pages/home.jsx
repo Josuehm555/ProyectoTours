0bb935062125d13cc97de04b96f50005627ed63b
Jeremmy Soto <jeremmysq17@gmail.com>
2022-08-28 06:21:27 +0000

    Admin service table

--- tour/src/pages/home.jsx
+++ tour/src/pages/home.jsx
@@ -1,9 +1,14 @@
-
+import Table from "../components/admin-table-3-columns/table";
+import Columns from "../api/category-table-columns.json"
+import Rows from '../api/categories.json'
 
 export default function Home() {
     return (
         <>
-            
+            {/* aqui va el buscador
+            aqui va el boton de agregar */}
+            <Table columns={Columns} rows={Rows}/>
+            {/* aqui va la paginacion */}
         </>
     );
 }
