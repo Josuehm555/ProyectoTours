d1d13c0383782e791c624d493c14a95147a0ced4
Jeremmy Soto <jeremmysq17@gmail.com>
2022-08-28 10:15:47 +0000

    Add button

---
+++ tour/src/components/add-button/button.jsx
@@ -0,0 +1,9 @@
+import './styles.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+export default function Button({titulo, url}) {
+    return (
+        <button><FontAwesomeIcon icon={faPlus}/> {titulo}</button>
+    );
+}
