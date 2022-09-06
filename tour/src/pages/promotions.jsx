a640997be85be42adc4187f409df4afa923c5b66
Josuehm555 <55064328+Josuehm555@users.noreply.github.com>
2022-09-05 21:03:10 -0600

    Promotions

---
+++ tour/src/pages/promotions.jsx
@@ -0,0 +1,32 @@
+import Columns from "../api/JSON-promotions/promotion-table-columns.json"
+import Rows from "../api/JSON-promotions/promotions.json"
+import TablePage from "../components/table-page/page";
+import { faEye, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import Add from "../components/modals/categories/add/add";
+import See from "../components/modals/categories/see/see";
+import Update from "../components/modals/categories/edit/edit";
+import Delete from "../components/modals/delete/delete";
+
+export default function Promotions() {
+
+    const Buttons = [ 
+        {title:"Abrir", style:{ color: "#aede67", marginLeft: "0" }, icon:faEye, titleModal: "Información de la Promoción"},
+        {title:"Modificar", style:{ color: "#3b97b7"}, icon:faEdit, titleModal: "Modificar Información"},
+        {title:"Eliminar", style:{ color: "red", marginRight: "0" }, icon:faTrash, titleModal: "Confirmación"}
+    ]
+
+    return (
+        <TablePage
+            Columns={Columns}
+            Rows={Rows}
+            Title={"Promociones"}
+            titleModal={"Información completa"}
+            messageButton={"Ok"} 
+            Buttons={Buttons}
+            Add={Add}
+            See={See}
+            Update={Update}
+            Delete={Delete}
+        />
+    );
+}
--- tour/src/route/route.jsx
+++ tour/src/route/route.jsx
@@ -4,6 +4,7 @@ import Admin from '../pages/admin';
 import NotFoundPage from '../pages/not-found-page';
 import Galery from '../pages/galery';
 import Subcategory from '../pages/subcategories';
+import Promotions from '../pages/promotions';
 
 export default function Routing() {
     return (
@@ -13,6 +14,7 @@ export default function Routing() {
             <Route path='/administradores' element={<Admin/>} />
             <Route path='/galeria' element={<Galery/>} />
             <Route path='/subcategoria' element={<Subcategory/>} />
+            <Route path='/promociones' element={<Promotions/>} />
         </Routes>
     );
 }
